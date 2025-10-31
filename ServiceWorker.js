8164f1cda95d86f3ce5a48aa189c95841ecf7d04
joi <doomster3445@gmail.com>
2025-10-31 23:07:09 +0800

    1

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -9,11 +9,11 @@ const contentToCache = [
 ];
 
 self.addEventListener('install', function (e) {
-    console.log('[Service Worker] Install');
+    //console.log('[Service Worker] Install');
     
     e.waitUntil((async function () {
       const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
+      //console.log('[Service Worker] Caching all: app shell and content');
       await cache.addAll(contentToCache);
     })());
 });
@@ -21,12 +21,12 @@ self.addEventListener('install', function (e) {
 self.addEventListener('fetch', function (e) {
     e.respondWith((async function () {
       let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      //console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (response) { return response; }
 
       response = await fetch(e.request);
       const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      //console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
       cache.put(e.request, response.clone());
       return response;
     })());
